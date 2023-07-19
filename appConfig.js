482c81381daa4d0aab5cd41c24a186c6165881b6
Mohammad Javad Khademian <mjkhonline@live.com>
2023-07-19 09:57:32 +0330

    Create appConfig.js

---
+++ appConfig.js
@@ -0,0 +1,25 @@
+export const BLOCKS_COUNT = 3
+
+export const INIT_CREDIT = 10
+
+export const ROLLING_COST = 1
+
+export const SUPPORTED_SYMBOLS = [
+    {
+        name: 'cherry',
+        reward: 10
+    },
+    {
+        name: 'lemon',
+        reward: 20
+    },
+    {
+        name: 'orange',
+        reward: 30
+    },
+    {
+        name: 'watermelon',
+        reward: 40
+    }
+]
+
