6e7c0d0711648551ff18021901e62186a427c0e0
Mohammad Javad Khademian <mjkhonline@live.com>
2023-07-19 11:15:37 +0330

    add JsDoc

--- appConfig.js
+++ appConfig.js
@@ -1,11 +1,31 @@
+/**
+ * The default number of blocks of the slot machine.
+ * @type {number}
+ */
 export const BLOCKS_COUNT = 3
 
+/**
+ * The default initial credit for the player.
+ * @type {number}
+ */
 export const INIT_CREDIT = 10
 
+/**
+ * The default cost of rolling the slots.
+ * @type {number}
+ */
 export const ROLLING_COST = 1
 
+/**
+ * The default delay in milliseconds before revealing each block.
+ * @type {number}
+ */
 export const REVEAL_DELAY = 1000
 
+/**
+ * The supported symbols in the game, with their name and reward value.
+ * @type {Array<{name: string, reward: number}>}
+ */
 export const SUPPORTED_SYMBOLS = [
   {
     name: 'cherry',
@@ -25,6 +45,10 @@ export const SUPPORTED_SYMBOLS = [
   }
 ]
 
+/**
+ * The rules for cheating, based on the player's credit.
+ * @type {Array<{condition: function(number): boolean, reRollProbability: number}>}
+ */
 export const CHEATING_RULES = [
   {
     condition: (credit) => credit < 40,
--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,37 @@
+/**
+ * Returns a promise that resolves after a specified number of milliseconds.
+ * @param {number} ms - The number of milliseconds to wait before resolving the promise.
+ * @returns {Promise<void>} A promise that resolves after the specified delay.
+ */
 export function asyncTimeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
+/**
+ * Returns a random integer between min (inclusive) and max (exclusive).
+ * @param {number} min - The minimum value (inclusive).
+ * @param {number} max - The maximum value (exclusive).
+ * @returns {number} A random integer between min and max.
+ */
 export function rnd(min, max) {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
+/**
+ * Returns true with the given probability.
+ * @param {number} prob - The probability, as a percentage, of returning true.
+ * @returns {boolean} True with the given probability, false otherwise.
+ */
 export function probability(prob) {
   return Math.random() < prob / 100
 }
 
+/**
+ * Returns a random position within a circle of the given radius.
+ * @param {number} len - The radius of the circle.
+ * @param {string} [unit='px'] - The unit to use for the position values.
+ * @returns {{x: string, y: string}} An object with x and y properties representing the random position.
+ */
 export function rndPosition(len, unit = 'px') {
   const angle = Math.random() * (2 * Math.PI)
 
@@ -19,6 +41,11 @@ export function rndPosition(len, unit = 'px') {
   }
 }
 
+/**
+ * Returns true if all items (primitive values) in the array are equal, false otherwise.
+ * @param {Array} arr - The array to check.
+ * @returns {boolean} True if all items in the array are equal, false otherwise.
+ */
 export function areAllItemsEqual(arr) {
   return arr.every((item) => item === arr[0])
 }
